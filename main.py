12efab8eccd313f57027e4a59aa77167edb46e69
Halley Harper <qwrty.streams@gmail.com>
2025-05-06 20:48:24 -0400

    added so much stuff

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import turtle
-from typedefs import Camera, Polygon, MAX_POLYS, Vec2
+from typedefs import Camera, Polygon, Vec2, ScreenSpacePoly
 from levelinit import Init
 from math import sin, cos
+import keyboard
+import time
+import numpy as np
 
+# constants
+MOV_SPEED = 100000
+ROT_SPEED = 500
+MAX_POLYS = 10
+MAX_VERTS = 8
+SHOULD_RASTERIZE = 1
+# decrease for better resolution; increase for better performance
+RASTER_RESOLUTION = 4
+RASTER_NUM_VERTS = 4
+
+# camera and polygons
 cam = Camera()
 polys = [Polygon() for _ in range(MAX_POLYS)]
 
@@ -10,18 +24,32 @@ polys = [Polygon() for _ in range(MAX_POLYS)]
 screen = turtle.Screen()
 screen.tracer(0)
 
+# globals
+loop = True
+
+delta_time: float = 0.0
+
+screenSpaceVisibilityPlanes: int = 0
+screenSpacePolys = [[ScreenSpacePoly() for _ in range(MAX_VERTS)] for _ in range(MAX_POLYS)]
+
+# screen info
 widthS = screen.window_width()
 heightS = screen.window_height()
 
-# Set the origin to the top-left corner
+# set the origin to the top-left corner
 screen.setworldcoordinates(0, heightS, widthS, 0)
 
 print(f"Width: {widthS}\nHeight: {heightS}")
+print(f"CamPos: {cam.camPos.x}, {cam.camPos.y}")
 
 t = turtle.Turtle()
 t.ht()
 
+# init the level
+Init(cam, polys)
+
 # floor / ceil func
+# currently unused
 def draw_half(color, y_start):
     t.goto(0, y_start)
     t.pendown()
@@ -38,6 +66,7 @@ def draw_half(color, y_start):
     t.penup()
 
 # draw pixel
+# currently unused
 def put_pixel(x, y, hex):
     if x > widthS or y > heightS or x < 0 or y < 0:
         return
@@ -65,12 +94,17 @@ def draw_line(x0, y0, x1, y1):
     t.goto(end)
     t.penup()
 
+# render shit
 def render():
+    t.clear()
     for polyIdx in range(MAX_POLYS):
         for vertIdx in range(polys[polyIdx].vertCnt - 1):
             p1: Vec2 = polys[polyIdx].vert[vertIdx]
             p2: Vec2 = polys[polyIdx].vert[vertIdx + 1]
             height: float = -polys[polyIdx].height
+            
+            if is_face_front(cam.camPos, p1, p2) > 0:
+                continue
             
             distX1: float = p1.x - cam.camPos.x
             distY1: float = p1.y - cam.camPos.y
@@ -83,6 +117,27 @@ def render():
             distX1 = distX1 * sin(cam.camAngle) - distY1 * cos(cam.camAngle)
             distX2 = distX2 * sin(cam.camAngle) - distY2 * cos(cam.camAngle)
             
+            if z1 > 0 or z2 > 0:
+                i1: Vec2 = intersection(distX1, z1, distX2, z1, -0.0001, 0.0001, -20, 5)
+                i2: Vec2 = intersection(distX1, z1, distX2, z1, 0.0001, 0.0001, 20, 5)
+                
+                if z1 <= 0:
+                    if i1.y < 0:
+                        distX1 = i1.x
+                        z1 = i1.y
+                    else:
+                        distX1 = i2.x
+                        z1 = i2.y
+                elif z2 <= 0:
+                    if i1.y > 0:
+                        distX2 = i1.x
+                        z2 = i1.y
+                    else:
+                        distX2 = i2.x
+                        z2 = i2.y
+            else:
+                continue
+            
             widthRatio: float = widthS / 2
             heightRatio: float = (widthS * heightS) / 60.0
             centerHeight: float = heightS / 2
@@ -94,17 +149,113 @@ def render():
             y1b: float = heightRatio / z1
             y2a: float = (height - heightRatio) / z2
             y2b: float = heightRatio / z2
+            
+            draw_line(centerWidth + x1, centerHeight + y1a, centerWidth + x2, centerHeight + y2a)
+            draw_line(centerWidth + x1, centerHeight + y1b, centerWidth + x2, centerHeight + y2b)
+            draw_line(centerWidth + x1, centerHeight + y1a, centerWidth + x1, centerHeight + y1b)
+            draw_line(centerWidth + x2, centerHeight + y2a, centerWidth + x2, centerHeight + y2b)
+ 
+# cross product of 2D points
+# fuck if i know how this works
+def cross_2d_points(x1, y1, x2, y2) -> float:
+    return x1 * y2 - y1 * x2
 
+# same thing with cross product but i dont know what it does
+def intersection(
+    x1, y1, x2, y2,
+    x3, y3, x4, y4) -> Vec2:
+    p: Vec2 = Vec2(0, 0)
+    
+    p.x = cross_2d_points(x1, y1, x2, y2)
+    p.y = cross_2d_points(x3, y3, x4, y4)
+    
+    det: float = cross_2d_points(x1 - x2, y1 - y2, x3 - x4, y3 - y4)
+    
+    p.x = cross_2d_points(p.x, x1 - x2, p.y, x3 - x4) / det
+    p.y = cross_2d_points(p.x, y1 - y2, p.y, y3 - y4) / det
+    
+    return p
 
-Init(cam, polys)
-      
-# draw here
-draw_half("#6d7178", heightS // 2)
-draw_half("#8c602a", heightS)
+# check if front face
+def is_face_front(Camera: Vec2, pointA: Vec2, pointB: Vec2) -> int:
+    localA = Vec2(pointA.x - Camera.x, pointA.y - Camera.y)
+    localB = Vec2(pointB.x - Camera.x, pointB.y - Camera.y)
+    
+    cross_product = localA.x * localB.y - localA.y * localB.x
+    
+    if cross_product > 0:
+        return 1
+    if cross_product < 0:
+        return -1
+    
+    return 0
 
-put_pixel(100, 100, "#0000ff")
-draw_line(0, 0, widthS, heightS)
+def camera_translate(delta_time: float):
+    if keyboard.is_pressed("w"):
+        cam.camPos.x += MOV_SPEED * cos(cam.camAngle) * delta_time
+        cam.camPos.y += MOV_SPEED * sin(cam.camAngle) * delta_time
+    elif keyboard.is_pressed("s"):
+        cam.camPos.x -= MOV_SPEED * cos(cam.camAngle) * delta_time
+        cam.camPos.y -= MOV_SPEED * sin(cam.camAngle) * delta_time
+    
+    if keyboard.is_pressed("a"):
+        cam.camAngle -= ROT_SPEED * delta_time
+    elif keyboard.is_pressed("d"):
+        cam.camAngle += ROT_SPEED * delta_time
+        
+    print(f"CamPos: {cam.camPos.x}, {cam.camPos.y}")
+    print(f"CamAngle: {cam.camAngle}")
 
-# draw to screen
-screen.update()
-screen.mainloop()
+def point_in_poly(nvert, vertx: float, verty: float, testx: float, testy: float) -> bool:
+    i = 0
+    j = 0
+    is_point_inside = False
+    
+    for i in range(nvert):
+        j = nvert - 1
+        
+        is_same_coordinates = False
+        
+        if (verty[i] > testy) == (verty[j] > testy):
+            is_same_coordinates = True
+        
+        if not is_same_coordinates and (testx < (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) + vertx[i]):
+            is_point_inside = not is_point_inside
+    
+    return is_point_inside
+
+def clear_raster_buffer():
+    for polyIdx in range(MAX_POLYS):
+        for i in range(polys[polyIdx].vertCnt):
+            for vn in range(RASTER_NUM_VERTS):
+                screenSpacePolys[polyIdx][i].vert[vn].x = 0
+                screenSpacePolys[polyIdx][i].vert[vn].y = 0
+                
+def rasterize():
+    vx = [0.0] * 4
+    vy = [0.0] * 4
+    
+    pixel_buff = np.zeros((widthS, heightS, 3), dtype=np.uint8)
+    pixel_buff.fill(0)
+    
+    # for 
+
+while loop:
+    if keyboard.is_pressed("esc"):
+        loop = False
+    
+    # start time
+    start: float = time.time()
+
+    # translate camera
+    camera_translate(delta_time)
+
+    # render the scene
+    render()
+
+    # draw to screen
+    screen.update()
+    
+    # end time
+    end: float = time.time()
+    delta_time = (end - start) / 1000.0
